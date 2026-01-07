1d8a455405d2b4143eee972d63406dd61905cda5
Havyner <havy012_@hotmail.com>
2026-01-07 20:04:23 -0300

    atividade16

---
+++ atividade16.py
@@ -0,0 +1,9 @@
+# Leia cinco números e armazene em uma lista
+
+lista = []
+
+for i in range(5):
+    numero = int(input('Informe um número: '))
+    lista.append(numero)
+
+print("Lista final: ", lista)
